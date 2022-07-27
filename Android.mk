7123ff7933cb5f60975cc4862f5279a9e19531ba
lopestorm <guillopes2002@gmail.com>
2022-07-26 22:51:24 -0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,5 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),WP16_RU)
+#ifeq ($(TARGET_DEVICE),WP16_RU)
+ifeq ($(PRODUCT_NAME),WP16_RU)
 include $(call all-subdir-makefiles,$(LOCAL_PATH))
 endif
