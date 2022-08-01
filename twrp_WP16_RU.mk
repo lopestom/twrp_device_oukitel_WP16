164d2447bf91eb6b52a4bf4ce5d648851655540c
lopestorm <guillopes2002@gmail.com>
2022-08-01 18:03:41 -0300

    Update twrp_WP16_RU.mk

--- twrp_WP16_RU.mk
+++ twrp_WP16_RU.mk
@@ -4,15 +4,15 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/base.mk)
 
-# Inherit from device/oukitel/WP16_RU device
-$(call inherit-product, device/oukitel/WP16_RU/device.mk)
+# Inherit from device/oukitel/WP16 device
+$(call inherit-product, device/oukitel/WP16/device.mk)
 
 # Inherit some common twrp stuff.
 $(call inherit-product, vendor/twrp/config/common.mk)
 
 # Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := WP16_RU
-PRODUCT_NAME := twrp_WP16_RU
+PRODUCT_DEVICE := WP16
+PRODUCT_NAME := twrp_WP16
 PRODUCT_BRAND := OUKITEL
 PRODUCT_MODEL := WP16
 PRODUCT_MANUFACTURER := OUKITEL
@@ -20,7 +20,7 @@ PRODUCT_RELEASE_NAME := OUKITEL WP16
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=WP16 \
-    BUILD_PRODUCT=OnePlus7 \
+    BUILD_PRODUCT=WP16_RU \
     PRODUCT_NAME=WP16_RU \
     PRIVATE_BUILD_DESC="full_lv978-user 11 RP1A.200720.011 20220618 release-keys"
 
